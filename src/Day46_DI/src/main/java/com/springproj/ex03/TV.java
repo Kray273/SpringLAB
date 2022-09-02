3679121266f9a7e504d0e7eff853970a9b86ce61
Kray <108450083+Kray273@users.noreply.github.com>
2022-09-02 18:31:44 +0900

    Hi

---
+++ src/Day46_DI/src/main/java/com/springproj/ex03/TV.java
@@ -0,0 +1,10 @@
+package com.springproj.ex03;
+
+public interface TV {
+	
+	public void powerOn();
+	public void powerOff();
+	public void volumeUp();
+	public void volumeDown();
+	
+}
---
+++ src/Day46_DI/src/main/java/com/springproj/ex03/TVuser.java
@@ -0,0 +1,24 @@
+package com.springproj.ex03;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TVuser {
+
+	public static void main(String[] args) {
+		
+		//TV tv = new LgTV(); 스프링이 관리함으로 필요 없어짐.
+
+		AbstractApplicationContext factory = new GenericXmlApplicationContext("applicationContext.xml");
+		//위의 작업을 할 수 있도록 스프링에게 요청
+		
+		TV tv = (TV)factory.getBean("tv"); //tv의 주소값을 요청
+		//강제 형변환이 필요_리턴값을 getBean이 Object로 보고 있다.  
+		
+		tv.powerOn();//기존의 코드를 그대로 사용. 
+		
+		factory.close(); // 사용한 메모리를 종료. 
+		
+		
+	}
+}
