c15b0ed18b1f9527ec13e1c8f4048cb277d0eb5d
Kray <108450083+Kray273@users.noreply.github.com>
2022-09-06 18:24:53 +0900

    Hi

---
+++ src/Day48_Project_Layer_BusinessComponent/src/main/java/com/springproj/biz/board/dao/BoardDAO.java
@@ -0,0 +1,29 @@
+package com.springproj.biz.board.dao;
+
+import com.springproj.biz.board.BoardVO;
+
+public class BoardDAO { //DAO_Data Access Object
+	
+	//CRUD의 method 구현
+	// 등록 (insert문)
+	public void insertBoaed(BoardVO vo) {
+		
+	}
+	//목록보기 (select문)
+	public void getBoaed(int seq) {
+		
+	}
+	
+	public void getBoaedList() {
+		
+	}
+	// 수정 (update문)
+	public void updateBoaed(BoardVO vo) {
+		
+	}
+	
+	// 삭제 (delete문)
+	public void deleteBoaed(int seq) {
+		
+	}
+}
