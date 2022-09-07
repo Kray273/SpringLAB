66ee7ea69471e393d1de748c7deb3956ccb52b2b
Kray <108450083+Kray273@users.noreply.github.com>
2022-09-07 16:57:49 +0900

    hi

---
+++ src/Day49_Project_Layer_BusinessComponent/src/main/java/com/springproj/biz/common/JDBCUtil.java
@@ -0,0 +1,39 @@
+package com.springproj.biz.common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JDBCUtil {
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","scott", "tiger");
+			return conn; 
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return null; //에러를 체크하기 위해 null을 반환
+	}
+	
+	public static void close(PreparedStatement pstmt, Connection conn) {
+		if(pstmt != null) {
+			try { 
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace(); 
+			}	
+		} 
+		if(conn != null) { 
+			try {
+		 	conn.close(); 
+		 	} catch (SQLException e) {
+		 		e.printStackTrace(); 
+	 		} 
+		}	
+	}
+	
+}
