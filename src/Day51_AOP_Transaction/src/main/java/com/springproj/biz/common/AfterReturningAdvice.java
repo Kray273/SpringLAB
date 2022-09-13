b9e39c8a923cc2aa8aa60c08c88343786c2c8df1
Kray <108450083+Kray273@users.noreply.github.com>
2022-09-13 15:41:25 +0900

    Hi

---
+++ src/Day51_AOP_Transaction/src/main/java/com/springproj/biz/common/AfterReturningAdvice.java
@@ -0,0 +1,35 @@
+package com.springproj.biz.common;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+import com.springproj.biz.board.BoardVO;
+
+@Service
+@Aspect
+public class AfterReturningAdvice {
+	
+	//@Pointcut("execution(* com.springproj.biz..*Impl.get*(..))")
+	//public void getPointcut() {} 
+		
+		//@AfterReturning(pointcut = "getPointcut()", returning = "returnObj")
+		//반환값을 처리해 줘야할때는 위와 같이 적용.
+	@AfterReturning(pointcut = "PointcutCommon.getPointcut()", returning = "returnObj")	
+	public void afterLog(JoinPoint jp, Object returnObj) {
+			String method = jp.getSignature().getName(); 
+			
+			if(returnObj instanceof BoardVO) {
+				BoardVO board = (BoardVO) returnObj;
+				if(board.getWriter().equals("euiseok")) {
+					System.out.println("This name is euiseok");
+				}
+			}
+			
+			System.out.println("[정상 종료(AfterReturn)] "+method+" 비지니스 로직 수행 후 동작");
+			
+		}
+			
+}
---
+++ src/Day51_AOP_Transaction/src/main/java/com/springproj/biz/common/AroundAdvice.java
@@ -0,0 +1,36 @@
+package com.springproj.biz.common;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
+
+@Service
+@Aspect
+public class AroundAdvice {
+		@Pointcut("execution(* com.springproj.biz..*Impl.*(..))")
+		public void allPointcut() {} 
+		
+		@Around("allPointcut()")
+		public Object aroundLog(ProceedingJoinPoint pjp) {
+			String method = pjp.getSignature().getName();
+			StopWatch stopWatch = new StopWatch();
+			stopWatch.start();
+		
+
+			try {
+				Object returnObj = pjp.proceed();
+				stopWatch.stop();
+				System.out.println(method +"() 메서드 수행 걸린 시간 :"+stopWatch.getTotalTimeMillis()+"(ms)초");
+				return returnObj;
+				
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+			
+}
